f213fe9a507d1571e8acd15821d59d5aac22c4c1
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2022-03-23 21:07:32 +0200

    Initial commit

---
+++ Out variables/AreaAndCirumferance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Out_variables
+{
+    internal class AreaAndCirumferance
+    {
+        public static double CalculateCircle(double radious,out double area)
+        {
+
+            area = (Math.PI * Math.Pow(radious, 2));
+            double circumference = 2 * Math.PI * radious;
+            return circumference;
+        }
+        public static void PrintStars(string s)
+        {
+            if (int.TryParse(s, out var i)) 
+                Console.WriteLine(new string('*', i)); 
+            else  
+                Console.WriteLine("Cloudy - no stars tonight!"); 
+        }
+
+    }
+}
---
+++ Out variables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+namespace Out_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Out version Out");
+            Write("enter radious ");
+            double radious=Convert.ToDouble(ReadLine());
+            double OldVerisonArea;
+            double circumference = AreaAndCirumferance.CalculateCircle(radious,  out OldVerisonArea);
+            WriteLine($"circumference is {circumference}");
+            WriteLine($"Area is {OldVerisonArea}");
+            ///<sammary>
+            ///common use of out parameters
+            /// </sammary>
+            /// 
+            AreaAndCirumferance.PrintStars("25");
+            WriteLine("new  version Out variables");
+            double newcircumference = AreaAndCirumferance.CalculateCircle(radious, out double newVersionArea);
+            WriteLine($"circumference is {newcircumference}");
+            WriteLine($"Area is {newVersionArea}");
+            
+
+
+        }
+
+
+    }
+}
